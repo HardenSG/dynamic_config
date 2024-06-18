bc4a0fdb6da4be3bee396e6932826b976f826c47
HardenSG <yandeqiang_i@didiglobal.com>
2024-06-18 21:28:43 +0800

    fix: 还原pm2启动路径

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'dynamic_config_sys',
-      script: './main.js',
+      script: 'dist/main.js',
       instances: '1',
       exec_mode: 'cluster',
       autorestart: true,
